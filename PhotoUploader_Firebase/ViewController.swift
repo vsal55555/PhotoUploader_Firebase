188fd882306b1aea16f85acdf9d1d0b8db186715
vsal55555 <vsal55555@gmail.com>
2020-07-29 22:09:30 +0545

    Initial Commit

---
+++ PhotoUploader_Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoUploader_Firebase
+//
+//  Created by BSAL-MAC on 7/29/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
